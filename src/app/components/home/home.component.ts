bdc9c619cb154a725cabe0abc915d3af8089c685
Nathan <nathan.tomines00@gmail.com>
2023-11-21 16:31:19 +0100

    messo contenuto in home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  constructor(protected authService : AuthService){}
 }
